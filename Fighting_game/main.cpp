0572d21d9e400fa621ef90e9d566eb5d60d566fa
Vladimir Stalmakov <akummail@gmail.com>
2013-08-31 16:37:49 +0400

    add sdl2

--- Fighting_game/main.cpp
+++ Fighting_game/main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#include "SDL2/SDL.h"
 
 using namespace std;
 
-int main()
+const int SCREEN_WIDTH  = 640;
+const int SCREEN_HEIGHT = 480;
+
+void logSDLError(std::ostream &os, const std::string &msg){
+	os << msg << " error: " << SDL_GetError() << std::endl;
+}
+
+#ifdef _WIN32
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpCmdLine, int nShowCmd)
+#else
+int main(int argc, char* argv[])
+#endif
 {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+	logSDLError(std::cout, "SDL_Init");
+	return 1;
+}
+
+SDL_Window *window = SDL_CreateWindow("Lesson 2", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT,
+	SDL_WINDOW_SHOWN);
+if (window == nullptr){
+	logSDLError(std::cout, "CreateWindow");
+	return 2;
+}
+SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,
+	SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+if (renderer == nullptr){
+	logSDLError(std::cout, "CreateRenderer");
+	return 3;
+}
     cout << "Hello world!" << endl;
     return 0;
 }
